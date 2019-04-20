f294ca04e5f43974e39dd1f21d40f6a6cfcb8011
marcosaugustomodesto <marcosaugustomodesto15@gmail.com>
2019-04-20 18:07:22 -0300

    inicial

---
+++ aluraframe/client/js/index.js
@@ -0,0 +1,44 @@
+var campos = [
+  document.querySelector('#data'),
+  document.querySelector('#quantidade'),
+  document.querySelector('#valor')  
+];
+
+console.log(campos);
+
+var tbody = document.querySelector('table tbody');
+
+
+document.querySelector('.form').addEventListener('submit', function(event) {
+    
+   event.preventDefault();
+   
+   var tr = document.createElement('tr');
+   
+   campos.forEach(function(campo) {
+       
+       var td = document.createElement('td');
+       td.textContent = campo.value;
+       tr.appendChild(td);
+   });
+   
+  var tdVolume = document.createElement('td');
+  tdVolume.textContent = campos[1].value * campos[2].value; 
+  
+  tr.appendChild(tdVolume);
+  
+  tbody.appendChild(tr);
+
+  let nome = 'marcos';
+  let nome = 'modesto';
+
+  console.log(nome);
+  
+  campos[0].value = '';
+  campos[1].value = 1;
+  campos[2].value = 0;
+  
+  campos[0].focus();
+
+   
+});
